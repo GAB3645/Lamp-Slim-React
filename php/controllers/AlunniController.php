3d004c61d2916837f65c0cd8828bd22abdd47060
GAB3645 <gabrielsiano2006@gmail.com>
2025-03-30 21:44:35 +0200

    first commit

--- php/controllers/AlunniController.php
+++ php/controllers/AlunniController.php
@@ -12,4 +12,40 @@ class AlunniController
     $response->getBody()->write(json_encode($results));
     return $response->withHeader("Content-type", "application/json")->withStatus(200);
   }
+
+  public function show(Request $request, Response $response, $args){
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+    $result = $mysqli_connection->query("SELECT * FROM alunni WHERE id =".$args['id']);
+    $result = $result->fetch_assoc();
+
+    $response->getBody()->write(json_encode($result));
+    return $response->withHeader("Content-type", "application/json")->withStatus(200);
+  }
+
+  public function create(Request $request, Response $response, $args){
+    $data = $request->getParsedBody();
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+    $result = $mysqli_connection->query("INSERT INTO alunni (nome, cognome, email) VALUES ('".$data['nome']."','".$data['cognome']."','".$data['email']."')");
+
+    $response->getBody()->write("Alunno creato correttamente");
+    return $response->withHeader("Content-type", "application/json")->withStatus(201);
+  }
+
+  public function update(Request $request, Response $response, $args){
+    $data = $request->getParsedBody();
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+    $result = $mysqli_connection->query("UPDATE alunni SET nome = '".$data['nome']."', cognome = '".$data['cognome']."', email = '".$data['email']."' WHERE id =".$args['id']);
+
+    $response->getBody()->write("Alunno aggiornato correttamente");
+    return $response->withHeader("Content-type", "application/json")->withStatus(200);
+  }
+
+  public function delete(Request $request, Response $response, $args){
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+    $result = $mysqli_connection->query("DELETE FROM alunni WHERE id =".$args['id']);
+
+    $response->getBody()->write("Alunno eliminato correttamente");
+    return $response->withHeader("Content-type", "application/json")->withStatus(200);
+  }
 }
+
